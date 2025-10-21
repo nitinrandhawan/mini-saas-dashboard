209584f0749c6ab4bc7d90d99b8658f0314a45ce
nitinrandhawan <nitinrandhawan@gmail.com>
2025-10-22 00:22:54 +0530

    set up frontend & backend

---
+++ backend/server.js
@@ -0,0 +1,34 @@
+import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./config/db.config.js";
+dotenv.config();
+import cors from "cors";
+
+const app = express();
+if (process.env.NODE_ENV === "development") {
+  const { default: morgan } = await import("morgan");
+  app.use(morgan("dev"));
+}
+
+const allowedOrigins = ["http://localhost:3000", "http://localhost:3001"];
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
+
+connectDB();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
---
+++ frontend/src/app/page.js
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button"
+
+export default function Home() {
+  return (
+    <>
+    <h1 className="text-4xl font-bold text-blue-500">Tailwind is working!</h1>
+    <Button className={"mt-4"}>Click me</Button>
+    </>
+  );
+}
